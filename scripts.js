eb9cbab111cbebc3ed9cd23e6b02b0164960d268
Grazieli <grazielimartins5@gmail.com>
2025-02-27 16:15:36 -0300

    site

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function toggleLangMenu() {
+    document.getElementById("fullscreenLang").classList.toggle("active");
+}
+
+function toggleMobileMenu() {
+    var menu = document.getElementById("mobileMenu");
+    menu.classList.toggle("active");
+}
